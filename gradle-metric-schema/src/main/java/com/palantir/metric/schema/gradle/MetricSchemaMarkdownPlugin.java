d4dcebfe7a110468323e216b51e0972ed50f7c47
Carter Kozak <ckozak@ckozak.net>
2020-07-23 16:45:36 -0400

    regenerating+checking in one run fails the check

--- gradle-metric-schema/src/main/java/com/palantir/metric/schema/gradle/MetricSchemaMarkdownPlugin.java
+++ gradle-metric-schema/src/main/java/com/palantir/metric/schema/gradle/MetricSchemaMarkdownPlugin.java
@@ -34,20 +34,20 @@ public final class MetricSchemaMarkdownPlugin implements Plugin<Project> {
         TaskProvider<CreateMetricsManifestTask> createMetricsManifest =
                 project.getTasks().named(MetricSchemaPlugin.CREATE_METRICS_MANIFEST, CreateMetricsManifestTask.class);
 
-        TaskProvider<GenerateMetricMarkdownTask> generateMetricsMarkdown = project.getTasks()
-                .register(MetricSchemaPlugin.GENERATE_METRICS_MARKDOWN, GenerateMetricMarkdownTask.class, task -> {
-                    task.getManifestFile().set(createMetricsManifest.flatMap(CreateMetricsManifestTask::getOutputFile));
-                    task.getOutputFile().set(project.file("metrics.md"));
-                    task.dependsOn(createMetricsManifest);
-                });
-
         TaskProvider<CheckMetricMarkdownTask> checkMetricsMarkdown = project.getTasks()
                 .register(MetricSchemaPlugin.CHECK_METRICS_MARKDOWN, CheckMetricMarkdownTask.class, task -> {
                     task.getManifestFile().set(createMetricsManifest.flatMap(CreateMetricsManifestTask::getOutputFile));
                     task.setMarkdownFile(project.file("metrics.md"));
                     task.dependsOn(createMetricsManifest);
+                });
+
+        project.getTasks()
+                .register(MetricSchemaPlugin.GENERATE_METRICS_MARKDOWN, GenerateMetricMarkdownTask.class, task -> {
+                    task.getManifestFile().set(createMetricsManifest.flatMap(CreateMetricsManifestTask::getOutputFile));
+                    task.getOutputFile().set(project.file("metrics.md"));
+                    task.dependsOn(createMetricsManifest);
                     // Avoid a race when both checkMetricsMarkdown and generateMetricsMarkdown are requested
-                    task.mustRunAfter(generateMetricsMarkdown);
+                    task.mustRunAfter(checkMetricsMarkdown);
                 });
 
         project.getTasks().named("check", check -> check.dependsOn(checkMetricsMarkdown));
