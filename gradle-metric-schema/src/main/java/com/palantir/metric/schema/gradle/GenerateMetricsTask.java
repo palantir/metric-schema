92bf21fea83b5f9f2f9808b337c176690e2ed57c
Mark Elliot <markelliot@users.noreply.github.com>
2025-03-20 10:55:13 -0400

    Remove use of getProject() from task execution

--- gradle-metric-schema/src/main/java/com/palantir/metric/schema/gradle/GenerateMetricsTask.java
+++ gradle-metric-schema/src/main/java/com/palantir/metric/schema/gradle/GenerateMetricsTask.java
@@ -48,6 +48,9 @@ public abstract class GenerateMetricsTask extends DefaultTask {
     public abstract RegularFileProperty getInputFile();
 
     @Input
+    public abstract Property<String> getProjectGroup();
+
+    @Input
     @org.gradle.api.tasks.Optional
     public abstract Property<String> getLibraryName();
 
@@ -62,7 +65,6 @@ public abstract class GenerateMetricsTask extends DefaultTask {
     public final void generate() {
         File output = getOutputDir().getAsFile().get();
         clearOutput(output.toPath());
-        getProject().mkdir(output);
 
         JavaGenerator.generate(JavaGeneratorArgs.builder()
                 .input(getInputFile().getAsFile().get().toPath())
@@ -70,7 +72,7 @@ public abstract class GenerateMetricsTask extends DefaultTask {
                 .libraryName(Optional.ofNullable(getLibraryName().getOrNull()))
                 .libraryVersion(Optional.ofNullable(getLibraryVersion().getOrNull()))
                 // TODO(forozco): probably want something better
-                .defaultPackageName(getProject().getGroup().toString())
+                .defaultPackageName(getProjectGroup().get())
                 .build());
     }
 
