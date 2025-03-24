75e54c3df538cae7b57b1d2d0bb10e5d48575404
Mark Elliot <mark.elliot@gmail.com>
2025-03-24 12:15:59 -0400

    Update gradle-metric-schema/src/main/java/com/palantir/metric/schema/gradle/MetricSchemaPlugin.java
    Co-authored-by: David Schlosnagle <schlosna@gmail.com>

--- gradle-metric-schema/src/main/java/com/palantir/metric/schema/gradle/MetricSchemaPlugin.java
+++ gradle-metric-schema/src/main/java/com/palantir/metric/schema/gradle/MetricSchemaPlugin.java
@@ -74,7 +74,7 @@ public final class MetricSchemaPlugin implements Plugin<Project> {
                     task.getLibraryName().convention(defaultLibraryName(project));
                     task.getLibraryVersion().convention(defaultLibraryVersion(project));
                     task.getOutputDir().set(generatedJavaDir);
-                    task.getProjectGroup().set(project.getGroup().toString());
+                    task.getPackageName().set(project.getGroup().toString());
                 });
 
         project.getTasks().register(CreateMetricsManifestTask.NAME, CreateMetricsManifestTask.class, task -> {
